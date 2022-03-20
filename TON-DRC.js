a39cd6d1048c6084db61444e36c24e719eac998c
hyp cos <hypcosine@gmail.com>
2022-03-21 00:04:07 +0800

    last Taranovsky's (binary) systems

---
+++ TON-DRC.js
@@ -0,0 +1,89 @@
+//Encoding: -1 for '0', 0 for 'W', [b,a,-2] for C(a,b), x[1] for left part, x[0] for right part
+//In reflection configuration, -0.5 for 'x'
+;var DRCStd = {}
+register.push({
+   id:'ton-drc'
+   ,name:'Degrees of Reflection (reflection configuration)'
+   ,able:TON_limit
+   ,compare:TON_noraise_compare
+   ,display:TON_noraise_display
+   ,FS:(term,n)=>{
+      var smallpart = term=>{
+         var sow_smallpart = a=>{
+            if(a===0) return;
+            if(TON_noraise_compare(a,0)<0){
+               result.push(a)
+            }else{
+               sow_smallpart(a[0])
+               sow_smallpart(a[1])
+            }
+         }
+         ,result=[]
+         sow_smallpart(term)
+         return result
+      }
+      ,BuiltQ = (a,ap,b,x,xp)=>TON_noraise_compare(x,0)<0
+         ? TON_noraise_compare(x,b)<0||TON_noraise_compare(r(x,xp),r(a,ap))<=0&&BuiltQ(a,ap,b,x[0],x)&&BuiltQ(a,ap,b,x[1],x)
+         : x===0||BuiltQ(a,ap,b,x[0],xp)&&BuiltQ(a,ap,b,x[1],xp)
+      ,StandardQ = a=>{
+         var str = JSON.stringify(a)
+         if(DRCStd[str]){
+            return DRCStd[str]
+         }else if(typeof a==='number' || (StandardQ(a[1])&&StandardQ(a[0]) && (typeof a[0]==='number'||TON_noraise_compare(a[1],a[0][1])<=0) &&
+         smallpart(a[1]).every(x=>BuiltQ(x,a,a,x,a)))){
+            return DRCStd[str]=true
+         }else{
+            return false
+         }
+      }
+      ,Copy = x=>typeof x==='number'?x:[Copy(x[0]),Copy(x[1]),-2]
+      if(''+term==='Infinity'){
+         term = [-1,0,-2]
+      }
+      var flag,c1,c3
+      ,beta = Copy(term)
+      ,lim = (''+term).split(',').length + n*2
+      do{
+         flag=false;
+         if(typeof beta==='number'&&beta>=0){					   //gamma = 'W', m = 0, assuming beta != '0'
+            beta=-1
+         }else if(beta[1]===-1){		                           //gamma = '0', m = 1
+            beta=beta[0]
+            flag=true
+            continue
+         }else if(typeof beta[1]==='number'&&beta[1]>=0){		//gamma = 'W', m = 1
+            beta[1]=-1
+         }else if(beta[1][1]===-1){                            //gamma = '0', m = 2
+            beta=[[beta[0],beta[1][0],-2],0,-2]
+         }else if(typeof beta[1][1]==='number'&&beta[1][1]>=0){//gamma = 'W', m = 2
+            beta[1][1]=-1
+         }else{							                           //m > 2, the main part of step 1 and 2
+            c3=beta
+            c1=beta[1][1]
+            while(typeof c1[1]!=='number'){
+               c3=c3[1]
+               c1=c1[1]
+            }
+            if(c1[1]===-1){                                    //gamma = '0', m > 2
+               c3[1]=[[c3[1][0],c1[0],-2],0,-2]
+            }else{							                        //gamma = 'W', m > 2
+               c1[1]=-1
+            }
+         }
+         while((''+beta).split(',').length<lim&&StandardQ(beta)){//better step 3
+            if(typeof beta!=='number'){
+               c1=beta
+               while(typeof c1[1]!=='number')c1=c1[1]		      //step 4
+               c1[1]=[c1[1],0,-2]									   //step 5
+            }else{
+               beta=[beta,0,-2]										//step 4 and 5 for beta = '0' and 'W'
+            }
+         }
+      }while(flag||!StandardQ(beta));
+      return beta
+   }
+   ,init:()=>[
+      {expr:Infinity,low:[-1],subitems:[]}
+      ,{expr:-1,low:[-1],subitems:[]}
+   ]
+})
---
+++ TON-DRPC.js
@@ -0,0 +1,94 @@
+//Encoding: -1 for '0', 0 for 'W', [b,a,-2] for C(a,b), x[1] for left part, x[0] for right part
+//In reflection configuration, -0.5 for 'x'
+;var DRPCStd = {}
+register.push({
+   id:'ton-drpc'
+   ,name:'Degrees of Reflection with Passthrough (reflection configuration)'
+   ,able:TON_limit
+   ,compare:TON_noraise_compare
+   ,display:TON_noraise_display
+   ,FS:(term,n)=>{
+      var smallpart = term=>{
+         var sow_smallpart = a=>{
+            if(a===0) return;
+            if(TON_noraise_compare(a,0)<0){
+               result.push(a)
+            }else{
+               sow_smallpart(a[0])
+               sow_smallpart(a[1])
+            }
+         }
+         ,result=[]
+         sow_smallpart(term)
+         return result
+      }
+      ,BuiltQ = (a,ap,ai,b,a0,d)=>{
+         if(a===0||TON_noraise_compare(a,b)<0) return true
+         if(d===-1&&TON_noraise_compare(a,0)<0&&TON_noraise_compare(r(a,ap),r(ai,b))>0) return false
+         if(TON_noraise_compare(a,d)<0) return BuiltQ(a,ap,ai,b,a0,-1)
+         var x2 = TON_noraise_compare(a,0)<0?a:ap
+         if(d===-1&&TON_noraise_compare(a[0],0)<0&&TON_noraise_compare(r(a[1],x2),r(a0,b))<0) return BuiltQ(a,ap,ai,b,a0,a)
+         return BuiltQ(a[0],x2,ai,b,a0,d)&&BuiltQ(a[1],x2,ai,b,a0,d)
+      }
+      ,StandardQ = a=>{
+         var str = JSON.stringify(a)
+         if(DRPCStd[str]){
+            return DRPCStd[str]
+         }else if(typeof a==='number' || (StandardQ(a[1])&&StandardQ(a[0]) && (typeof a[0]==='number'||TON_noraise_compare(a[1],a[0][1])<=0) &&
+         smallpart(a[1]).every(x=>BuiltQ(x,a,x,a,a[1],-1)))){
+            return DRPCStd[str]=true
+         }else{
+            return false
+         }
+      }
+      ,Copy = x=>typeof x==='number'?x:[Copy(x[0]),Copy(x[1]),-2]
+      if(''+term==='Infinity'){
+         term = [-1,0,-2]
+      }
+      var flag,c1,c3
+      ,beta = Copy(term)
+      ,lim = (''+term).split(',').length + n*2
+      do{
+         flag=false;
+         if(typeof beta==='number'&&beta>=0){					   //gamma = 'W', m = 0, assuming beta != '0'
+            beta=-1
+         }else if(beta[1]===-1){		                           //gamma = '0', m = 1
+            beta=beta[0]
+            flag=true
+            continue
+         }else if(typeof beta[1]==='number'&&beta[1]>=0){		//gamma = 'W', m = 1
+            beta[1]=-1
+         }else if(beta[1][1]===-1){                            //gamma = '0', m = 2
+            beta=[[beta[0],beta[1][0],-2],0,-2]
+         }else if(typeof beta[1][1]==='number'&&beta[1][1]>=0){//gamma = 'W', m = 2
+            beta[1][1]=-1
+         }else{							                           //m > 2, the main part of step 1 and 2
+            c3=beta
+            c1=beta[1][1]
+            while(typeof c1[1]!=='number'){
+               c3=c3[1]
+               c1=c1[1]
+            }
+            if(c1[1]===-1){                                    //gamma = '0', m > 2
+               c3[1]=[[c3[1][0],c1[0],-2],0,-2]
+            }else{							                        //gamma = 'W', m > 2
+               c1[1]=-1
+            }
+         }
+         while((''+beta).split(',').length<lim&&StandardQ(beta)){//better step 3
+            if(typeof beta!=='number'){
+               c1=beta
+               while(typeof c1[1]!=='number')c1=c1[1]		      //step 4
+               c1[1]=[c1[1],0,-2]									   //step 5
+            }else{
+               beta=[beta,0,-2]										//step 4 and 5 for beta = '0' and 'W'
+            }
+         }
+      }while(flag||!StandardQ(beta));
+      return beta
+   }
+   ,init:()=>[
+      {expr:Infinity,low:[-1],subitems:[]}
+      ,{expr:-1,low:[-1],subitems:[]}
+   ]
+})
